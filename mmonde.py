c4e77f24b8a916b7ae206f9a66e7c73fd9673b95
maxgigi <max.grdt@gmail.com>
2024-09-27 17:33:28 +0200

    ajout fichier mressources : contient la classe Ressource et initialise wood et gold include mressources dans mmonde pour tester

--- mmonde.py
+++ mmonde.py
@@ -1,9 +1,11 @@
 #Créé par Max le 27/09/2024
+from mressources import *
 
 class Tuile:
     def __init__(self,id):
         self.id=id
-        self.ressource="O"
+        self.ressource=" " #" " représente une tuile sans ressources, sera remplacé par une instance de Ressource
+        #il suffira de changer le __repr__ de Ressource pour afficher la lettre correspondant à la ressource
         self.unites={} #les cles seront les ids et les valeurs seront les instances Unite
 
     def __repr__(self):
@@ -18,7 +20,7 @@ class Monde:
         self.dico={} #à chaque clé sera associé une Tuile
         #les clés du dico seront de la forme "0,0"
 
-    def creer_monde(self):
+    def creer_monde(self): #remplit de Tuile le dico
         for x in range(self.x):
             for y in range(self.y):
                 cle=(x,y)
@@ -29,7 +31,16 @@ class Monde:
             for y in range(self.y):
                 print(self.dico[(x,y)],end="")
             print("",end="\n")
+'''
+monde=Monde((5,20))
+monde.creer_monde()
 
-#monde=Monde((5,20))
-#monde.creer_monde()
-#monde.afficher_console()
+for i in range(1,3):
+    for j in range(4,13):
+        monde.dico[(i,j)]=wood
+
+for j in range(2,9):
+    monde.dico[(4,j)]=gold
+
+monde.afficher_console()
+'''
