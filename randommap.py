66025877026d8830652af9fb7330a4696cfb30e5
maxgigi <max.grdt@gmail.com>
2024-09-27 18:54:01 +0200

    maps aléatoires good exécutez tests si ça vous fait plaisir c'est mimi je vous jure

--- randommap.py
+++ randommap.py
@@ -9,18 +9,28 @@ def somme(t1,t2): #somme((1,1),(2,2))=(3,3)
         total+=(t1[i]+t2[i],) #virgule permet d'avoir un tuple
     return total
 
-def placer(monde,cle,ressource): #place un cluster d'une ressource autour d'une tuile
+def placer(monde,cle,ressource,chance=50): #place un cluster d'une ressource autour d'une tuile d'un monde
     #place le bloc initial
     monde.dico[cle]=ressource
     #place peut être à côté, récursif
     for i in range(-1,2):
         for j in range(-1,2):
-            if(randint(0,100)>85):
-                placer(monde,somme((i,j),cle),ressource)
+            if(randint(0,100)<=chance): #50% réplication -16% à chaque réplication
+                placer(monde,somme((i,j),cle),ressource,chance-16)
 
-def randomise(monde): #prend un monde, fait placer() plusieurs fois sur des cercles autour du centre
-    x_centre=monde.x//2
-    y_centre=monde.y//2
+def randomise(monde,richesse): #prend un monde, place des clusters de ressources sur un cercle, +de clusters selon la richesse
+    #blocs de wood
+    for i in range(3+richesse*2):
+        x=min(randint(0,monde.x),randint(0,monde.x)) #faire cela permet d'éloigner les blocs du centre
+        y=min(randint(0,monde.y),randint(0,monde.y))
+        placer(monde,(x,y),wood)
+        placer(monde,(monde.x-x,monde.y-y),wood) #symétrie centrale
+    #blocs de gold
+    for i in range(1 + richesse):
+        x = min(randint(0, monde.x), randint(0, monde.x))
+        y = min(randint(0, monde.y), randint(0, monde.y))
+        placer(monde, (x, y), gold)
+        placer(monde, (monde.x - x, monde.y - y), gold)
 
 '''
 print(somme((1,1),(2,2)))
--- tests.py
+++ tests.py
@@ -1,3 +1,11 @@
 #Créé par Max le 27/09/2024
 
-import
+from randommap import *
+
+monde=Monde(120,120)
+monde.creer_monde()
+
+randomise(monde,2)
+placer(monde,(60,60),gold,80)
+
+monde.afficher_console()
