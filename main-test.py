1b33f67936b1c04dda0faeff44dece29e0920f65
etan-test1 <etandu0627@gmail.com>
2024-10-11 15:49:33 +0200

    Merge max et début affichage

---
+++ main-test.py
@@ -0,0 +1,22 @@
+import pygame as pg
+from game import Game
+
+def main () :
+    running = True
+    playing = True
+
+    pg.init()
+    pg.mixer.init()
+    screen = pg.display.set_mode((0,0), pg.FULLSCREEN)
+    clock = pg.time.Clock()
+    game = Game(screen, clock)
+
+
+    while running :
+
+        while playing :
+
+            game.run()
+
+if __name__ == "__main__" :
+    main()
--- mmonde.py
+++ mmonde.py
@@ -1,5 +1,6 @@
 #Créé par Max le 27/09/2024
 from mressources import *
+from .setup import TILE_SIZE
 
 class Tuile:
     def __init__(self,id):
@@ -7,18 +8,31 @@ class Tuile:
         self.ressource=" " #" " représente une tuile sans ressources, sera remplacé par une instance de Ressource
         #il suffira de changer le __repr__ de Ressource pour afficher la lettre correspondant à la ressource
         self.unites={} #les cles seront les ids et les valeurs seront les instances Unite
+        self.rect = [
+            (id[0] * TILE_SIZE, id[1] * TILE_SIZE),
+            (id[0] * TILE_SIZE + TILE_SIZE, id[1] * TILE_SIZE),
+            (id[0] * TILE_SIZE + TILE_SIZE, id[1] * TILE_SIZE + TILE_SIZE),
+            (id[0] * TILE_SIZE, id[1] * TILE_SIZE + TILE_SIZE)
+        ]
+        self.iso = [self.cart_to_iso(id[0],id[1]) for id[0],id[1] in self.rect]
 
     def __repr__(self):
         if self.unites!={}:
             return self.unites
         return self.ressource
 
+    def cart_to_iso (self, x, y):
+        iso_x = x-y
+        iso_y = (x + y)/2
+        return iso_x, iso_y
+
 class Monde:
     def __init__(self,x,y): #x et y dimensions du monde
         self.x=x
         self.y=y
         self.dico={} #à chaque clé sera associé une Tuile
-        #les clés du dico seront de la forme "0,0"
+        #les clés du dico seront de la forme "(0,0)"
+        self.creer_monde()
 
     def creer_monde(self): #remplit de Tuile le dico
         for x in range(self.x):
@@ -31,6 +45,7 @@ class Monde:
             for y in range(self.y):
                 print(self.dico[(x,y)],end="")
             print("",end="\n")
+
 '''
 monde=Monde(5,20)
 monde.creer_monde()
