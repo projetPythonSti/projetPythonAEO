2fc6db21b6e70a92e371e869411aedb9dcdf2d19
Moutanazhir <adam.errais07@gmail.com>
2024-10-25 15:25:45 +0200

    Update buildings.py

--- buildings.py
+++ buildings.py
@@ -1,1 +1,18 @@
-print("Hello World")
+class Buildings:
+    def _init_(self, cost: int, time_building: float, health: int, surface: int, spawn: 'Unity', drop_point: bool):
+        self.cost = cost
+        self.time_building = time_building
+        self.health = health
+        self.surface = surface
+        self.spawn = spawn
+        self.drop_point = drop_point
+
+
+    def build(self, builders_list: list):
+        total_build_speed = sum([builder.building_speed for builder in builders_list])
+        build_time_remaining = self.time_building
+
+        while build_time_remaining > 0:
+            build_time_remaining -= total_build_speed
+
+        print("Building completed!")
