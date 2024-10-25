0915e95940434ef70c529bcb3e1194e71e184da9
Moutanazhir <adam.errais07@gmail.com>
2024-10-25 17:37:40 +0200

    Create stable.py

---
+++ stable.py
@@ -0,0 +1,12 @@
+from buildings import Building
+
+class Stable(Building) :
+  def __init__(self) :
+    super().__init(
+      name="Stable",
+      cost={"wood": 175},
+      time_building=50,
+      health=500,
+      surface=9,  # 3x3
+      spawn="Horseman"
+    )
