69f113ea460d5d8e6fd7b2ae443da2f8ea32aacc
tkdev1755 <khetibh@gmail.com>
2024-11-17 11:22:46 +0100

    Finished Adding game handling files from etandu branch

---
+++ guiTests.py
@@ -0,0 +1,46 @@
+import pygame as pg
+import time
+import os, sys
+from game import Game
+from randommap import *
+
+def jeu_terminal (world):
+    running = True
+    playing = True
+    pg.init()
+    pg.mixer.init()
+    screen = pg.display.set_mode((0, 0), pg.FULLSCREEN)
+    clock = pg.time.Clock()
+    game = Game(screen, clock,world)
+
+    while running :
+
+        while playing :
+            game.run_console()
+
+
+
+def main (world) :
+    running = True
+    playing = True
+
+    pg.init()
+    pg.mixer.init()
+    screen = pg.display.set_mode((0,0), pg.FULLSCREEN)
+    clock = pg.time.Clock()
+    game = Game(screen, clock,world)
+
+
+    while running :
+
+        while playing :
+
+            game.run()
+
+if __name__ == "__main__" :
+    monde = World(10, 10)
+    monde.creer_monde()
+    randomise(monde, 3)
+    print("hi")
+    #main()
+    jeu_terminal(monde)
