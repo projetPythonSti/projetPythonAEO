6f9c09f7a1e27b876040035bf2099c6839953dbd
maxgigi <max.grdt@gmail.com>
2024-11-29 14:51:27 +0100

    maps now generate with town centers and their villagers

--- randommap.py
+++ randommap.py
@@ -1,5 +1,8 @@
 from mmonde import *
 from random import *
+from town_center import TownCenter
+from Archer import Archer
+from Unity import *
 
 def somme(t1,t2): #somme((1,1),(2,2))=(3,3)
     total=()
@@ -33,8 +36,23 @@ def randomise(monde,richesse): #prend un monde, place des clusters de ressources
         placer(monde, (monde.x-1-x,monde.y-1-y), Gold(), 30, 9)
     #gold au centre
     placer(monde, (monde.x // 2, monde.y // 2), Gold(), 80,40)
-    #
-
+    #clear some space for town centers, far from center
+    x = randint(1, (monde.x - 1) // 3)
+    y = randint(1, (monde.y - 1) // 3)
+    for i in range(-1,5):
+        for j in range(-1,5):
+            monde.dico[x+i,y+j].contains=" "
+            monde.dico[monde.x-x-i-1, monde.y-y-j-1].contains = " "
+    #towns centers not adjacent to borders
+    monde.spawn_building(TownCenter,"blue",x,y)
+    monde.spawn_building(TownCenter, "red", monde.x-x-4, monde.y-y-4)
+    #villagers adjacent to town centers
+    monde.spawn_unit(Archer,"blue",x,y+4)
+    monde.spawn_unit(Archer, "blue", x + 1, y+4)
+    monde.spawn_unit(Archer, "blue", x + 2, y+4)
+    monde.spawn_unit(Archer, "red", monde.x-x-4, monde.y-y-5)
+    monde.spawn_unit(Archer, "red", monde.x-x-3, monde.y-y-5)
+    monde.spawn_unit(Archer, "red", monde.x-x-2, monde.y-y-5)
 
 '''
 print(somme((1,1),(2,2)))
--- tests.py
+++ tests.py
@@ -10,14 +10,7 @@ monde.creer_monde()
 
 randomise(monde,2)
 
-a1b = monde.spawn_unit(Archer, "blue",1,2)
-a2b = monde.spawn_unit(Archer,"blue",2,2)
-a1r = monde.spawn_unit(Archer,"red",2,3)
-a2r = monde.spawn_unit(Archer,"red",3,3)
-
 monde.update_unit_presence()
-
-tc1b = monde.spawn_building(TownCenter, "blue", 5,2)
 
 monde.update_build_presence()
 
