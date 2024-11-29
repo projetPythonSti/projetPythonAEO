68888c13f29ddaa36591e08268e961da50350818
tkdev1755 <khetibh@gmail.com>
2024-11-29 14:22:51 +0100

    Fixed pathfinding.py

--- main-test.py
+++ main-test.py
@@ -2,32 +2,37 @@ import pygame as pg
 import time
 import os, sys
 
-from Archer import Archer
+
 from game import Game
+from game_term import Game_term
 from randommap import *
+from Archer import *
+import asyncio
 
 def jeu_terminal (world):
     running = True
     playing = True
-    clock = 0
-    game = Game(None, clock,world)
+    pg.init()
+    pg.mixer.init()
+    clock = pg.time.Clock()
+    game_term = Game_term(clock,world)
 
     while running :
 
         while playing :
-            game.run_console()
+            game_term.run_term()
 
 
 
-def main (world) :
+def jeu_pygame (world) :
     running = True
     playing = True
 
-    #pg.init()
-    #pg.mixer.init()
-    screen = pg.display.set_mode((0,0), )
+    pg.init()
+    pg.mixer.init()
+    screen = pg.display.set_mode((0,0), pg.FULLSCREEN)
     clock = pg.time.Clock()
-    game = Game(None, clock,world)
+    game = Game(screen, clock,world)
 
 
     while running :
@@ -35,6 +40,10 @@ def main (world) :
         while playing :
 
             game.run()
+
+def main () :
+    pass
+
 
 if __name__ == "__main__" :
     monde = World(100, 100)
@@ -44,5 +53,7 @@ if __name__ == "__main__" :
     a1 = Archer(1,1)
     a1.position = (1, 2)
     monde.units.append(a1)
+
     #main()
-    jeu_terminal(monde)
+    asyncio.run(jeu_terminal(monde))
+
