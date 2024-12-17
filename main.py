b6d7f682ee7e30306c3a15fb23e1c36c8ac7cff0
etan-test1 <etandu0627@gmail.com>
2024-12-17 14:57:05 +0100

    remove useless file before clean branch

--- main.py
+++ main.py
@@ -1,14 +1,56 @@
-from Horseman import Horseman
-from Archer import Archer
+import pygame as pg
+import time
+import os, sys
+
+from game import Game
+from game_term import Game_term
+from randommap import *
+from Archer import *
+import asyncio
+
+def jeu_terminal (world):
+    running = True
+    playing = True
+    clock = pg.time.Clock()
+    game_term = Game_term(world,clock)
+
+    while running :
+
+        while playing :
+            game_term.run_term()
 
 
-if __name__ == "__main__":
-    horseman = Horseman("h1");
-    archer = Archer("a1");
-    
-    horseman1 = Horseman("h2");
-    archer1 = Archer("a2");
-    
-    print(horseman, archer)
-    print(horseman == Archer)
-    
+
+def jeu_pygame (world) :
+    running = True
+    playing = True
+
+    pg.init()
+    pg.mixer.init()
+    screen = pg.display.set_mode((0,0), pg.FULLSCREEN)
+    clock = pg.time.Clock()
+    game = Game(screen, clock,world)
+
+
+    while running :
+
+        while playing :
+
+            game.run()
+
+def main () :
+    pass
+
+
+if __name__ == "__main__" :
+    monde = World(100, 100)
+    monde.creer_monde()
+    randomise(monde, 3)
+
+    a1 = Archer(1)
+    a1.position = (1, 2)
+    monde.units.append(a1)
+
+    #main()
+    jeu_terminal(monde)
+
