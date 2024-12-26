8c27843605fab447afeb9b092504d2723cf15c44
Moutanazhir <adam.errais07@gmail.com>
2024-12-26 15:00:38 +0100

    Update house.py

--- house.py
+++ house.py
@@ -3,7 +3,7 @@ from building import Building
 class House(Building):
     def __init__(self):
         super().__init__(
-            name="House",
+            name="H",
             cost={"wood": 25},
             time_building=25,
             health=200,
