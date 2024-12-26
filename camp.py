9e8f7c92b2255931c27a5073291e02ad61f9d577
Moutanazhir <adam.errais07@gmail.com>
2024-12-26 15:01:51 +0100

    Update camp.py

--- camp.py
+++ camp.py
@@ -3,7 +3,7 @@ from buildings import Building
 class Camp(Building) :
    def __init__(self):
         super().__init__(
-           name="Camp",
+           name="C",
            cost={"wood": 100},
            time_building=25,
            health=200,
