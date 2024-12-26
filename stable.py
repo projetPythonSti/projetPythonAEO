bbf44d0779ad7dbc432fe8640f3e71079f4cc65e
Moutanazhir <adam.errais07@gmail.com>
2024-12-26 15:01:03 +0100

    Update stable.py

--- stable.py
+++ stable.py
@@ -3,7 +3,7 @@ from buildings import Building
 class Stable(Building) :
   def __init__(self) :
     super().__init(
-      name="Stable",
+      name="S",
       cost={"wood": 175},
       time_building=50,
       health=500,
