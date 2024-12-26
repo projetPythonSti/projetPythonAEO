e40540f71b66dde2b9748f4eaae4662c7a789478
tkdev1755 <khetibh@gmail.com>
2024-12-26 13:57:23 +0100

    Finished checkUnitToMove and moveUnit() However, problems on time accuracy

--- logicTests.py
+++ logicTests.py
@@ -1,4 +1,6 @@
+import time
 from datetime import datetime
+import timeit
 
 from models.Position import Position
 from models.maps.Tile import Tile
@@ -36,18 +38,28 @@ if __name__ == "__main__":
     #print(community)
     gm = GameManager(speed=1, world=monde)
     print("Launched GameManager")
-    #gm.addUnitToMoveDict(v, Position(40, 40))
+
+    gm.addUnitToMoveDict(v, Position(40, 40))
     print("Added unit to move dict")
     gm.addUnitToMoveDict(community["sm"]["eq1p4"], Position(10,20))
     print("Added 2nd unit to move dict")
-    #print(gm.checkUnitsToMove())
+    #gm.checkUnitsToMove()
     #Boucle pour tester le game manager
-    n = 0
 
-    while n<500:
+    gm.tick = timeit.default_timer()
+    time.sleep(2)
+    # Record the start time
+    print("started main loop")
+    start_time = timeit.default_timer()
+    elapsedTime = 0
+    while True:
+        gm.tick = timeit.default_timer()
+        elapsedTime += (timeit.default_timer() - start_time)
+        print(elapsedTime)
+        if elapsedTime > 1.25:
+            print("Time's up! Stopping the loop.")
+            break
         gm.checkUnitsToMove()
-        tick = datetime.now()
-        n += 1
 
-    # print(monde.filled_tiles, len(monde.filled_tiles))
+
     # monde.show_world()
--- models/gameManager.py
+++ models/gameManager.py
@@ -1,15 +1,15 @@
+import timeit
 from collections import defaultdict
 from datetime import datetime
-
-from pygame.examples.music_drop_fade import starting_pos
-
+import time
+import re
 from models.Pathfinding import Pathfinding
 from models.World import World
 from models.unity.Unity import Unity
 
 
 class GameManager:
-        tick = datetime.today()
+        tick = timeit.default_timer()
         unitToMove = defaultdict(dict)
         '''
             Syntaxe du dictionnaire
@@ -27,25 +27,28 @@ class GameManager:
             self.gameSpeed = speed
             self.world = world
 
+        def getTeamNumber(self, name):
+            pattern = r'\d+'
+            substrings = re.findall(pattern, name)
+            print(substrings)
+            return int(substrings[0])
+
         def moveUnit(self, id):
+            deltaTime = timeit.default_timer() - self.tick
             unit = self.unitToMove[id]
-            time = datetime.today() - self.tick
-            milli_sec = (datetime.today().microsecond.real - self.tick.microsecond.real) // 1000
-            print(milli_sec)
-            print("time elapsed : ", unit["timeElapsed"])
-            unit["timeElapsed"] += milli_sec
-            if (unit["timeElapsed"] >= (unit["timeToTile"]*1000)):
-                print("Got to the next tile in", unit["timeElapsed"])
-                unit["timeElapsed"] = 0
+            unit["timeElapsed"] += deltaTime.real
+            #print("time elapsed : ", unit["timeElapsed"])
+            if unit["timeElapsed"] >= (unit["timeToTile"]):
                 unit["moveQueue"] = unit["moveQueue"][1::]
+                self.world.tiles_dico[(unit["currentTile"][0],unit["currentTile"][1])].contains = ""
+                self.world.tiles_dico[(unit["nextTile"][0], unit["nextTile"][0])].contains = self.world.villages[(unit["team"]-1)].community[id]
                 unit["currentTile"] = unit["moveQueue"][0]
-                print(unit["moveQueue"])
+                print("Got to the next tile in", (unit["timeElapsed"]))
+                unit["timeElapsed"] = 0
                 if (len(unit["moveQueue"]) < 2):
                     unit["moveQueue"] = []
                 else:
                     unit["nextTile"] = unit["moveQueue"][1]
-
-
 
 
         def checkUnitsToMove(self):
@@ -64,21 +67,25 @@ class GameManager:
 
         def addUnitToMoveDict(self, unit : Unity, destination):
             grid = self.world.convertMapToGrid()
+            teamNumber = self.getTeamNumber(unit.uid)
             pathFinding  = Pathfinding(mapGrid=grid, statingPoint= (unit.position.getX(), unit.position.getY()), goal=(destination.getX(), destination.getY()))
             path = pathFinding.astar()
             if path.__class__ == bool:
                 print("Found no short path")
             path = path + [pathFinding.startingPoint]
             path = path[::-1]
+
             self.unitToMove[unit.uid] = {
                 "group" : [],
-                "timeToTile" : 1/(unit.speed*self.gameSpeed),
+                "timeToTile" : 1/(unit.speed),
                 "timeElapsed" : 0,
                 "nextTile" : path[1],
                 "currentTile": path[0],
                 "estimatedPos" : path[0],
+                "team": teamNumber,
                 "moveQueue": path,
             }
+
 
 
 
