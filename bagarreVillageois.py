cd6ecabbaaf44132b487cbf463ab68257c0b343b
maxgigi <max.grdt@gmail.com>
2025-01-04 14:20:20 +0100

    fichier bagarreVillageois qui permettra à des villageois de s'entretuer, afin de montrer que tout ce qu'on a produit jusque là sert à qlqc

--- bagarreVillageois.py
+++ bagarreVillageois.py
@@ -9,3 +9,25 @@ from mmonde import *
 
 import time
 
+print("\n\n\n\n\n")
+
+#creates a 12 by 12 world for demonstration purposes
+monde=World(12,12)
+monde.creer_monde()
+#surrounds the world with wood for better console visibility
+for i in range(0,12):
+    monde.dico[(i,0)].contains = Wood()
+    monde.dico[(0, i)].contains = Wood()
+    monde.dico[(i, 11)].contains = Wood()
+    monde.dico[(11, i)].contains = Wood()
+#spawns 2 teams of 5 villagers each
+for i in range(5):
+    monde.spawn_unit(Villager, "blue", 3+i, 2)
+    monde.spawn_unit(Villager, "red", 4+i, 9)
+monde.afficher_console()
+#villagers should now target one of the opposing team villagers
+for u in monde.units:
+    ()
+#each cycle, every villager either moves towards its target or attacks it (if at range)
+#also, they should check if their target is alive. If not,
+
