97a3fed4c5934a84b3bfa0bc9c61ed2e87d64cf6
Moutanazhir <adam.errais07@gmail.com>
2025-01-07 15:59:06 +0100

    Update stable.py

--- stable.py
+++ stable.py
@@ -1,12 +1,13 @@
 from buildings import Building
 
-class Stable(Building) :
-  def __init__(self) :
-    super().__init(
-      name="S",
-      cost={"wood": 175},
-      time_building=50,
-      health=500,
-      longueur=3,
-      spawn="Horseman"
-    )
+class Stable(Building):
+    def __init__(self):
+        super().__init(
+            name="S",
+            cost={"w": 175},
+            time_building=50,
+            health=500,
+            max_health=500,
+            longueur=3,
+            spawn="h"
+        )
