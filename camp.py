013c2ac2f9f311ae08ee637432a24a94af22e1d1
Moutanazhir <adam.errais07@gmail.com>
2025-01-07 16:00:58 +0100

    Update camp.py

--- camp.py
+++ camp.py
@@ -1,13 +1,13 @@
 from buildings import Building
 
-class Camp(Building) :
-   def __init__(self):
+class Camp(Building):
+    def __init__(self):
         super().__init__(
-           name="C",
-           cost={"wood": 100},
-           time_building=25,
-           health=200,
-           longueur=2,
-           dropPoint=True
-       )
-          
+            name="C",
+            cost={"w": 100},
+            time_building=25,
+            health=200,
+            max_health=200,
+            longueur=2,
+            dropPoint=True
+        )
