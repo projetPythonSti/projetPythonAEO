40d0a4d00579f5331059d344f1bc74c159c5cca9
Moutanazhir <adam.errais07@gmail.com>
2025-01-07 16:00:21 +0100

    Update farm.py

--- farm.py
+++ farm.py
@@ -1,12 +1,13 @@
 from buildings import Building
 
-class Farm(Building) :
-  def __init__(self) :
-    super().__init__(
-      name="F",
-      cost={"Wood": 60},
-      build_time=10,
-      hp=100
-      longueur=2,
-      dropPoint=True
-    )
+class Farm(Building):
+    def __init__(self):
+        super().__init__(
+            name="F",
+            cost={"w": 60},
+            build_time=10,
+            health=100,
+            max_health=100,
+            longueur=2,
+            dropPoint=True
+        )
