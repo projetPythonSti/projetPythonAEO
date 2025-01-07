a85b9f6d34eb4017de6ed7e165be95557aa13bc6
Moutanazhir <adam.errais07@gmail.com>
2025-01-07 16:01:53 +0100

    Update barracks.py

--- barracks.py
+++ barracks.py
@@ -1,12 +1,13 @@
 from buildings import Building
 
-class Barracks(Building) :
-  def __init__(self) :
-    super().__init__(
-      name="B",
-      cost={"wood": 175},
-      time_building=50,
-      health=500,
-      longueur=3,
-      spawn="Swordsman"
-    )
+class Barracks(Building):
+    def __init__(self):
+        super().__init__(
+            name="B",
+            cost={"w": 175},
+            time_building=50,
+            health=500,
+            max_health=500,
+            longueur=3,
+            spawn="s"
+        )
