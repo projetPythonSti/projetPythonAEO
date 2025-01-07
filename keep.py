37043f3c3a40b8847277519277fc6a96ab1d50ff
Moutanazhir <adam.errais07@gmail.com>
2025-01-07 15:59:32 +0100

    Update keep.py

--- keep.py
+++ keep.py
@@ -1,11 +1,12 @@
 from buildings import Building
 
-class Keep(Building) :
-  def __init__(self) :
-    super().__init__(
-      name="K",
-      cost={"wood": 35, "gold": 125},
-      time_building=80,
-      health=800,
-      longueur=1,
-    )
+class Keep(Building):
+    def __init__(self):
+        super().__init__(
+            name="K",
+            cost={"w": 35, "g": 125},
+            time_building=80,
+            health=800,
+            max_health=800,
+            longueur=1,
+        )
