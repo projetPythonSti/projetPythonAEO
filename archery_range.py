87e8fb84eb46a77949d583a1b47d3fc3118aac16
Moutanazhir <adam.errais07@gmail.com>
2025-01-07 16:02:50 +0100

    Update archery_range.py

--- archery_range.py
+++ archery_range.py
@@ -1,12 +1,13 @@
 from buildings import Building
 
-class Archery_range(Building) :
-  def __init__(self) :
-    super().__init__(
-      name="A",
-      cost={"wood": 175},
-      time_building=50,
-      health=500,
-      longueur=3,
-      spawn="Archer"
-    )
+class Archery_range(Building):
+    def __init__(self):
+        super().__init__(
+            name="A",
+            cost={"w": 175},
+            time_building=50,
+            health=500,
+            max_health=500,
+            longueur=3,
+            spawn="a"
+        )
