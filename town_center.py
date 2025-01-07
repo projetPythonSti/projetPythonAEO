0f1e25b9c411c58eeaf8e1b7155e50d6233764f2
Moutanazhir <adam.errais07@gmail.com>
2025-01-07 15:58:32 +0100

    Update town_center.py

--- town_center.py
+++ town_center.py
@@ -4,11 +4,12 @@ class TownCenter(Building):
     def __init__(self):
         super().__init__(
             name="T",
-            cost={"wood": 350},
+            cost={"w": 350},
             time_building=150,
             health=1000,
+            max_health=1000,
             longueur=4,
             population=5,
             dropPoint=True,
-            spawn="Villager"
+            spawn="v"
         )
