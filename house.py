02982b4f91565086c8ffc9a9cd271b8b4091af1e
Moutanazhir <adam.errais07@gmail.com>
2025-01-07 15:57:34 +0100

    Update house.py

--- house.py
+++ house.py
@@ -4,9 +4,10 @@ class House(Building):
     def __init__(self):
         super().__init__(
             name="H",
-            cost={"wood": 25},
+            cost={"w": 25},
             time_building=25,
             health=200,
+            max_health=200,
             longueur=2,
             population=5
         )
