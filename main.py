0c307ce3728113426070a4d5eb458371caf8f5af
Moutanazhir <adam.errais07@gmail.com>
2025-01-08 22:19:59 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,46 @@
-print("hello world")
+wood = Wood()
+gold = Gold()
+food = Food()
+resources1 = [wood, gold, food]
+ressources2 = [wood, gold, food]
+
+class Population :
+    def __init__(self, population) :
+        self.population = population
+
+villagers1 = Population(23)
+villagers2 = Population(15)
+
+# Création de la keep
+keep = Keep()
+
+keep.build(resources, population.population)
+
+# Afficher les ressources restantes
+print("\nRessources restantes :")
+for res in resources1:
+    print(f"{res.name}: {res.getQuantity()}")
+
+# Simuler des dégâts
+keep.health -= 100  # Le bâtiment perd 10 points de santé
+print(f"Santé actuelle de la keep : {keep.health}/{keep.max_health}")
+
+# Réparer la maison
+keep.repair(resources, population.population)
+
+
+# Vérifier la santé du bâtiment
+print(f"Santé actuelle de la keep : {keep.health}/{keep.max_health}")
+
+#test de attack et take_damage
+
+house = House()
+
+house.build(resources, villagers.population)
+
+print("\nRessources restantes :")
+for res in resources1:
+    print(f"{res.name}: {res.getQuantity()}")
+
+while house.health != 0 :
+    keep.attack(house)
