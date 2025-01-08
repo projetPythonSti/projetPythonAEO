dbd11db34689437eb01b9fbd9b32f12a54e97373
Moutanazhir <adam.errais07@gmail.com>
2025-01-08 16:12:37 +0100

    Update keep.py

--- keep.py
+++ keep.py
@@ -1,7 +1,7 @@
 from buildings import Building
 
-class Keep(Building):
-    def __init__(self):
+class Keep(Building) :
+    def __init__(self) :
         super().__init__(
             name="K",
             cost={"w": 35, "g": 125},
@@ -9,4 +9,14 @@ class Keep(Building):
             health=800,
             max_health=800,
             longueur=1,
-        )
+          )
+        #self.attack_damage = 50
+
+    def attack(self, target):
+        if self.health != self.max_health :
+            print(f"{self.name} ne peut pas attaquer.")
+            return False
+
+        print(f"{self.name} attaque la cible {target.name} et inflige {self.attack_damage} points de dégâts.")
+        target.take_damage(self.attack_damage)
+        return True
