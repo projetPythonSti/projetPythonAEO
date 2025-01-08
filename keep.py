4b1e8c461802d5d12ff5a8e20e4a13fc150c10b8
Moutanazhir <adam.errais07@gmail.com>
2025-01-08 22:46:52 +0100

    Update keep.py

--- keep.py
+++ keep.py
@@ -11,13 +11,23 @@ class Keep(Building) :
             longueur=1,
           )
         #self.attack_damage = 50 par exemple
-        #self.attack_range = 10 "toute les unités dans cette range peuvent être attaqué
+        #self.attack_range = 10 "toute les unités dans cette range peuvent être attaqué"
+
+   def calculate_distance(self, target):
+        dx = self.position.getX() - target.position.getX()
+        dy = self.position.getY() - target.position.getY()
+        return math.sqrt(dx**2 + dy**2)
 
     def attack(self, target):
-        if self.health != self.max_health :
-            print(f"{self.name} ne peut pas attaquer.")
+        if self.health != self.max_health:
+            print(f"{self.name} ne peut pas attaquer car il n'est pas en pleine santé.")
             return False
 
-        print(f"{self.name} attaque la cible {target.name} et inflige {self.attack_damage} points de dégâts.")
-        target.take_damage(self.attack_damage)
-        return True
+        distance = self.calculate_distance(target)
+        if distance <= self.attack_range:
+            print(f"{self.name} attaque la cible {target.name} et inflige {self.attack_damage} points de dégâts.")
+            target.take_damage(self.attack_damage)
+            return True
+        else:
+            print(f"{self.name} ne peut pas attaquer {target.name} car hors attaque_range")
+            return False
